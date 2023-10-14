3b949b64af7918700fbc6c70ac008e9eb761f8c8
Ikuti <m.j.olkiewicz@gmail.com>
2023-10-14 20:00:02 +0200

    Basic player controller, follow main camera and some tweaks in enviroment

---
+++ GodotProject/core/camera/main_camera.gd
@@ -0,0 +1,13 @@
+@icon("res://core/camera/cctv-camera.svg")
+class_name MainCamera
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

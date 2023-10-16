72d02041f92da2c7cd7b4e068f65ca45618f0227
Ktosox <ktoso3@gmail.com>
2023-10-17 00:17:42 +0200

    First working version of crafting
    - updated how items work
    - added a crafting system
    - added a debug item spawning scene
    - added some place holder iamges for testing

--- GodotProject/entities/items/item_base.gd
+++ GodotProject/entities/items/item_base.gd
@@ -1,15 +1,40 @@
 extends RigidBody3D
 
-@onready var baseColor = $Sprite3D.modulate 
+@onready var baseColor = typeToColorMap[type]
 
-enum ITEM_TYPE {Herb, Pumpkin}
+enum ITEM_TYPE {Apple, Pumpkin, Mandrake, Mushroom, Flower, Pie, Cake, BadApple, Junk}
 
 @export var type : ITEM_TYPE
 
 @export var isReagent = true
 
+var typeToImageMap = {
+	0:load("res://entities/items/apple-seeds.png"),
+	1:load("res://entities/items/pumpkin.png"),
+	2:load("res://entities/items/tree-face.png"),
+	3:load("res://entities/items/mushroom-gills.png"),
+	4:load("res://entities/items/flowers.png"),
+	5:load("res://entities/items/pie-slice.png"),
+	6:load("res://entities/items/cake-slice.png"),
+	7:load("res://entities/items/apple-core.png"),
+	8:load("res://icon.png")
+}
+
+var typeToColorMap = {
+	0:Color("Red"),
+	1:Color("Orange"),
+	2:Color("DarkGray"),
+	3:Color("DarkGray"),
+	4:Color("LightBlue"),
+	5:Color("Orange"),
+	6:Color("Green"),
+	7:Color("Green"),
+	8:Color("White")
+}
+
+
 func _ready():
-	
+	$Sprite3D.texture = typeToImageMap[type]
 	pass
 
 func select_is_valid():
--- GodotProject/ktosox/debug_item_spawner.gd
+++ GodotProject/ktosox/debug_item_spawner.gd
@@ -1,15 +1,40 @@
 extends Node3D
 
-
+var itemScene = preload("res://entities/items/item_base.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func spawn_new_item(ID):
+	var newItem = itemScene.instantiate()
+	newItem.type = ID
+	get_parent().add_child(newItem)
+	return newItem
 	pass
 
 
-func _on_area_3d_body_entered(body):
+
+func _on_apple_spawner_body_entered(body):
+	spawn_new_item(0).global_position = body.global_position + Vector3(0,2,0)
+	pass # Replace with function body.
+
+
+func _on_mushroom_spawner_body_entered(body):
+	spawn_new_item(3).global_position = body.global_position + Vector3(0,2,0)
+	pass # Replace with function body.
+
+
+func _on_mandrake_spawner_body_entered(body):
+	spawn_new_item(2).global_position = body.global_position + Vector3(0,2,0)
+	pass # Replace with function body.
+
+
+func _on_flower_spawner_body_entered(body):
+	spawn_new_item(4).global_position = body.global_position + Vector3(0,2,0)
+	pass # Replace with function body.
+
+
+func _on_pumpkin_spawner_body_entered(body):
+	spawn_new_item(1).global_position = body.global_position + Vector3(0,2,0)
 	pass # Replace with function body.

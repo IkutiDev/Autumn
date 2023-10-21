8a7d7ee7633ff67c66faeb892550300ac2cb015e
Ikuti <m.j.olkiewicz@gmail.com>
2023-10-21 20:03:48 +0200

    Added in some world light + customers queue

--- GodotProject/characters/clients/client.gd
+++ GodotProject/characters/clients/client.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+signal remove_customer(customer)
 
 @export var desiredItem : PackedScene
 
@@ -13,29 +14,38 @@ var broughtItemNode
 
 @export var allItemsWanted : Array[PackedScene] = []
 
+@export_group("References")
+@export var animation_player : AnimationPlayer
+
 
 func _ready():
 	if desiredItem == null:
 		desiredItem = allItemsWanted[randi()%allItemsWanted.size()]
 	if broughtItem == null:
 		broughtItem = allItemsOffered[randi()%allItemsOffered.size()]
-	print(desiredItem,broughtItem)
+
 	desiredItemNode = desiredItem.instantiate()
 	broughtItemNode = broughtItem.instantiate()
+	print(desiredItemNode,broughtItemNode)
 	broughtItemNode.freeze = true
 	desiredItemNode.freeze = true
 	$DesiredItemPreview.add_child(desiredItemNode)
 	$OfferedItemPreview.add_child(broughtItemNode)
 
 
-
+func update_animation(is_walking := false) -> void:
+	if is_walking:
+		animation_player.play("walk")
+	else:
+		animation_player.play("idle")
 
 
 func give_gift():
 	broughtItemNode.reparent(get_tree().current_scene)
 	broughtItemNode.global_position = $ItemSpawnPoint.global_position
 	broughtItemNode.freeze = false
-	$AnimationPlayer.play("exit")
+	remove_customer.emit(self)
+	#$AnimationPlayer.play("exit")
 	pass
 
 
---
+++ GodotProject/world/customer_queue.gd
@@ -0,0 +1,50 @@
+extends Node3D
+
+@export var customer_scene : PackedScene
+@export var customer_speed := 1.0
+@export var customer_spawn_time := 10.0
+@export var customer_distance := 0.1
+
+@export_group("References")
+@export var path3D : Path3D
+
+var active_customers : Array[PathFollow3D]
+var customer_spawn_timer := 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	customer_spawn_timer = customer_spawn_time
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	for i in active_customers.size():
+		var customer = active_customers[i]
+		if customer.progress_ratio < 1.0 - i * customer_distance:
+			customer.get_child(0).update_animation(true)
+			customer.progress_ratio += delta * customer_speed
+			customer.progress_ratio = min(customer.progress_ratio, 1.0 - i * customer_distance)
+		if customer.progress_ratio + 0.01 >= 1.0 - i * customer_distance:
+			if customer.get_child(0) != null:
+				customer.get_child(0).update_animation(false)
+		
+	
+	if active_customers.size() >= 10:
+		return
+	
+	if customer_spawn_timer > 0:
+		customer_spawn_timer -= delta
+		if customer_spawn_timer <= 0:
+			var path_follower = PathFollow3D.new()
+			path3D.add_child(path_follower)
+			path_follower.loop = false
+			path_follower.rotation_mode = PathFollow3D.ROTATION_Y
+			var customer = customer_scene.instantiate()
+			path_follower.add_child(customer)
+			customer.remove_customer.connect(remove_customer)
+			active_customers.append(path_follower)
+			customer_spawn_timer = customer_spawn_time
+
+func remove_customer(customer : Node3D) -> void:
+	active_customers.erase(customer.get_parent())
+	customer.get_parent().queue_free()

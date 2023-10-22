3cf06bb0f1cf79211d3053a6c149a2c2ceed7790
Ikuti <m.j.olkiewicz@gmail.com>
2023-10-22 17:01:50 +0200

    Freeze time during summary screen

--- GodotProject/core/end_day_screen.gd
+++ GodotProject/core/end_day_screen.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 
-
+func _ready():
+	Engine.time_scale = 0
 
 
 func _on_button_pressed():
+	Engine.time_scale = 1
 	GameManager.end_night()
 	self.queue_free()
 	pass # Replace with function body.
